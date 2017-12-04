b3c3abc9a7c5e4186e7ac3f250bddc2a55638a99
lev.romanets <lev.romanets@worldapp.com>
2017-12-04 17:41:00 +0200

    countingSort4.

--- src/CountingSort4.java
+++ src/CountingSort4.java
@@ -3,29 +3,24 @@ import java.util.Scanner;
 
 public class CountingSort4 {
     public static void main( String[] args ) {
-        long st = System.currentTimeMillis();
+
         Scanner in = new Scanner( System.in);
         int max = 0;
-        int index;
-        String text;
+
         int s = in.nextInt();
-        String[] ar = new String[s];
+        StringBuilder[] ar = new StringBuilder[100];
         for(int i=0;i<s;i++){
-            index = in.nextInt();
-            text = in.next();
-            if (ar[index] == null) ar[index] = new String( "" );
-            if (i >= s / 2) ar[index] += text + " ";
-            else ar[index] += "- ";
+            int index = in.nextInt();
+            String text = in.next();
+            if (ar[index] == null) ar[index] = new StringBuilder( "" );
+            if (i >= s / 2) ar[index].append( text + " " );
+            else ar[index].append( "- ");
             if (max < index) max = index;
         }
 
-        long st1 = System.currentTimeMillis();
-
-        for ( String str :  Arrays.copyOf( ar, max + 1 )) {
+        for ( StringBuilder str :  Arrays.copyOf( ar, max + 1 )) {
             System.out.print(str);
         }
 
-        long end = System.currentTimeMillis();
-        System.out.println("First part: " + (st1-st) + ". Seconf part: " + (end-st1));
     }
 }
