5f788073e825db4f4db261d71160b3ddc1e9f747
lev.romanets <lev.romanets@worldapp.com>
2017-12-18 17:47:13 +0200

    gamingArray (><)

--- src/GamingArray.java
+++ src/GamingArray.java
@@ -7,32 +7,16 @@ public class GamingArray {
         int g = in.nextInt();
         for(int a0 = 0; a0 < g; a0++){
             int n = in.nextInt();
-            int[] A = new int[n];
-            for ( int i = 0; i < n; i++ ) {
-                A[i] = in.nextInt();
+            int winner = 0;
+            int max = 0;
+            for (int i = 0; i < n; i++) {
+                int number = in.nextInt();
+                if (max < number) {
+                    max = number;
+                    winner ^= 1;
+                }
             }
-
-            int bob = 0;
-            int end = n - 1;
-
-            do {
-                bob ^= 1;
-                end = maxId(A, end);
-            }while ( end > 0 );
-
-            System.out.println(bob == 1 ? "BOB" : "ANDY");
+            System.out.println(winner == 0 ? "ANDY" : "BOB");
         }
-    }
-
-    private static int maxId( int[] a, int end ) {
-        int maxi = 0;
-        int index = 0;
-        for ( int i = 0; i < end; i++ ) {
-            if (maxi < a[i]){
-                maxi = a[i];
-                index = i;
-            }
-        }
-        return index;
     }
 }
